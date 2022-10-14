98438ed0839831cad3a6abb2605a4010fcb58aba
Bnemoz <bnemoz2003@gmail.com>
2022-10-14 23:06:58 +0600

    Some changes were made by following PEP8 and DRY principles

--- tasksapp/views.py
+++ tasksapp/views.py
@@ -10,29 +10,28 @@ from .forms import TaskForm
 
 
 def is_get(request):
-    '''
-    Returns True if the request method is GET.
-    '''
+    '''Returns True if the request method is GET.'''
     return request.method == 'GET'
 
 
 def is_post(request):
-    '''
-    Returns True if the request method is POST.
-    '''
+    '''Returns True if the request method is POST.'''
     return request.method == 'POST'
 
 
 def home(request):
-    '''
-    Renders the homepage.
-    '''
+    '''Renders the homepage.'''
     return render(request, 'tasksapp/home.html')
 
 
 def sign_up_user(request):
+    '''Sign Up'''
     if is_get(request):
-        return render(request, 'tasksapp/sign_up_user.html', {'form': UserCreationForm()})
+        return render(
+            request,
+            'tasksapp/sign_up_user.html',
+            {'form': UserCreationForm()}
+        )
     else:
         username = request.POST['username']
         password1 = request.POST['password1']
@@ -40,23 +39,45 @@ def sign_up_user(request):
 
         passwords_match: bool = password1 == password2
         full_fields: bool = all((username, password1, password2))
+        eligible_password: bool = len(password1) >= 6
 
-        if passwords_match and full_fields and len(password1) >= 6:
+        if passwords_match and full_fields and eligible_password:
             try:
                 user = User.objects.create_user(
-                    username=username, password=password1)
+                    username=username,
+                    password=password1
+                )
                 user.save()
                 login(request, user)
                 return redirect('current_tasks')
             except IntegrityError:
-                return render(request, 'tasksapp/sign_up_user.html', {'error': 'The username is already taken. Please choose another one.', 'username': username})
+                return render(
+                    request, 
+                    'tasksapp/sign_up_user.html', 
+                    {'error': 'The username is already taken. Please choose another one.', 
+                    'username': username}
+                )
         else:
             if not passwords_match:
-                return render(request, 'tasksapp/sign_up_user.html', {'error': 'Passwords do not match.', 'username': username})
+                return render(
+                    request, 
+                    'tasksapp/sign_up_user.html', 
+                    {'error': 'Passwords do not match.', 
+                    'username': username}
+                )
             if not full_fields:
-                return render(request, 'tasksapp/sign_up_user.html', {'error': 'The fields must not be empty.'})
-            if not len(password1) >= 6:
-                return render(request, 'tasksapp/sign_up_user.html', {'error': 'Password must be equal or longer than 6 symbols.', 'username': username})
+                return render(
+                    request, 
+                    'tasksapp/sign_up_user.html', 
+                    {'error': 'The fields must not be empty.'}
+                )
+            if not eligible_password:
+                return render(
+                    request, 
+                    'tasksapp/sign_up_user.html', 
+                    {'error': 'Password must be equal or longer than 6 symbols.', 
+                    'username': username}
+                )
 
 
 def log_in_user(request):
@@ -71,9 +92,19 @@ def log_in_user(request):
         user = authenticate(request, username=username, password=password)
 
         if not full_fields:
-            return render(request, 'tasksapp/log_in_user.html', {'error': 'The fields must not be empty.', 'username': username})
+            return render(
+                request, 
+                'tasksapp/log_in_user.html', 
+                {'error': 'The fields must not be empty.', 
+                'username': username}
+            )
         if user is None:
-            return render(request, 'tasksapp/log_in_user.html', {'error': 'The user does not exist.', 'username': username})
+            return render(
+                request, 
+                'tasksapp/log_in_user.html', 
+                {'error': 'The user does not exist.', 
+                'username': username}
+            )
         else:
             login(request, user)
             return redirect('current_tasks')
@@ -89,7 +120,11 @@ def log_out_user(request):
 @login_required
 def create_task(request):
     if is_get(request):
-        return render(request, 'tasksapp/create_task.html', {'form': TaskForm()})
+        return render(
+            request, 
+            'tasksapp/create_task.html', 
+            {'form': TaskForm()}
+        )
     else:
         try:
             form = TaskForm(request.POST)
@@ -98,19 +133,36 @@ def create_task(request):
             new_task.save()
             return redirect('current_tasks')
         except ValueError:
-            return render(request, 'tasksapp/create_task.html', {'form': TaskForm(), 'error': 'Title\'s max length: 100. Memo\'s max length: 1000.'})
+            return render(
+                request, 
+                'tasksapp/create_task.html', 
+                {'form': TaskForm(), 
+                'error': 'Title\'s max length: 100. Memo\'s max length: 1000.'}
+            )
 
 
 @login_required
 def current_tasks(request):
     tasks = Task.objects.filter(user=request.user, completed_time__isnull=True)
-    return render(request, 'tasksapp/current_tasks.html', {'tasks': tasks})
+    return render(
+        request, 
+        'tasksapp/current_tasks.html', 
+        {'tasks': tasks}
+    )
 
 
 @login_required
 def completed_tasks(request):
-    tasks = Task.objects.filter(user=request.user, completed_time__isnull=False).order_by('-completed_time')
-    return render(request, 'tasksapp/completed_tasks.html', {'tasks': tasks})
+    tasks_filtered = Task.objects.filter(
+        user=request.user, 
+        completed_time__isnull=False
+    )
+    tasks = tasks_filtered.order_by('-completed_time')
+    return render(
+        request, 
+        'tasksapp/completed_tasks.html', 
+        {'tasks': tasks}
+    )
 
 
 @login_required
@@ -118,7 +170,14 @@ def task_page(request, task_pk):
     task = get_object_or_404(Task, pk=task_pk, user=request.user)
     if is_get(request):
         form = TaskForm(instance=task)
-        return render(request, 'tasksapp/task_page.html', {'task': task, 'form': form})
+        return render(
+            request, 
+            'tasksapp/task_page.html', 
+            {
+                'task': task, 
+                'form': form
+            }
+        )
     else:
         try:
             form = TaskForm(request.POST, instance=task)
@@ -128,7 +187,15 @@ def task_page(request, task_pk):
             else:
                 return redirect('completed_tasks')
         except ValueError:
-            return render(request, 'tasksapp/task_page.html', {'task': task, 'form': form, 'error': 'Title\'s max length: 100. Memo\'s max length: 1000.'})
+            return render(
+                request, 
+                'tasksapp/task_page.html', 
+                {
+                    'task': task, 
+                    'form': form, 
+                    'error': 'Title\'s max length: 100. Memo\'s max length: 1000.'
+                }
+            )
 
 
 @login_required
